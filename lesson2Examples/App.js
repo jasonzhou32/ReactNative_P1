765b01ab80da5c04251296c68f7cb1ac7e1d017f
Jason Z <zhoujason32@gmail.com>
2024-01-19 08:16:58 -0600

    Update App.js

--- lesson2Examples/App.js
+++ lesson2Examples/App.js
@@ -8,19 +8,22 @@ export default function App() {
     <View style={styles.container}>
       <View style = {styles.darkGreyContainer}>
         <View style = {styles.redSquare}>
-          <Text>1</Text>
+          <Text style = {{fontWeight:'bold'}}>1</Text>
         </View>
-      </View>
-      <View style = {styles.darkGreyContainer}>
         <View style = {styles.redSquare}>
-          <Text>2</Text>
+          <Text style = {{fontWeight:'bold'}}>2</Text>
         </View>
-      </View>
-      <View style = {styles.darkGreyContainer}>
         <View style = {styles.redSquare}>
-          <Text>3</Text>
+          <Text style = {{fontWeight:'bold'}}>3</Text>
         </View>
       </View>
+      {/* Dark Green Container */}
+      <View style = {styles.darkGreenContainer}>
+        <View style = {styles.lightGreenRectangle}></View>
+        <View style = {styles.lightGreenRectangle}></View>
+        <View style = {styles.lightGreenRectangle}></View>
+      </View>
+      
       
       <StatusBar style="auto" />
     </View>
@@ -33,24 +36,57 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
+    // justifyContent: 'center',
   },
   darkGreyContainer: {
     backgroundColor:'darkgray',
     flexDirection: 'row',
-    width: 100,
+    // flex: 2,
+    // without any other children of container 
+    // using flex, any number here will fill 
+    // the screen along the main axis
+    width: '80%',
+    height: '20%',
+    padding: 5,
+    justifyContent: 'center',
+    // alignItems:'center',
     // justifyContent: 'center',
 
   },
   redSquare: {
     backgroundColor:'red',
-    width: 20,
-    height: 20,
+    width: 40,
+    height: 40,
+    padding: 10,
+    margin: 10,
+    marginTop: 15,
+    justifyContent: 'center',
+    alignItems:'center',
   },
   darkGreenContainer: {
+    backgroundColor:'darkgreen',
+    flexDirection: 'column',
+    // flex: 2,
+    // without any other children of container 
+    // using flex, any number here will fill 
+    // the screen along the main axis
+    width: '80%',
+    height: '70%',
+    padding: 5,
+    marginTop: 15,
+    justifyContent: 'center',
+    alignItems:'center',
+    // justifyContent: 'center',
 
   },
   lightGreenRectangle :{
+    backgroundColor:'lightgreen',
+    width: '80%',
+    height: 50,
+    margin: 8,
+    justifyContent: 'center',
+    alignItems:'center',
 
   },
 });
