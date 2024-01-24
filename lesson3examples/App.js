92a09e23f9251c3ce5eb6f1128ee4653df1e1495
Jason Z <zhoujason32@gmail.com>
2024-01-24 08:12:18 -0600

    Imported image components.

--- lesson3examples/App.js
+++ lesson3examples/App.js
@@ -1,8 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight, Image, Alert} from 'react-native';
 
-
-
+import colors from './app/config/colors';
 /*
 const functionName = () => {
   function code
@@ -13,6 +12,12 @@ const myFirstFunction = () => {
   console.log("I wrote a function");
 }
 
+const myAlertFn  = () =>{
+  Alert.alert("Alert Title", "Alert message",[
+    {text: "yes", onPress: () => console.log("pressed yes")},
+    {text: "no", onPress: () => console.log("pressed no")},
+  ]);
+}
 
 export default function App() {
   return (
@@ -33,6 +38,31 @@ export default function App() {
         <Text>TH button</Text>
       </TouchableHighlight>
 
+      <TouchableHighlight
+        onPress={() => alert('Alert')}
+        style = {styles.customButton}
+        underlayColor='blue'
+      >
+        <Text>Custom button</Text>
+      </TouchableHighlight>
+
+      <Image
+        style = {styles.smallImageStyles}
+        source={{uri: 'https://picsum.photos/200/100'}}
+      
+      />
+
+      <Image
+        style = {styles.smallImageStyles}
+        source={require('./app/assets/images/pic1.jpg')}
+      
+      />
+
+      <Button
+        title="Alert button"
+        onPress={myAlertFn}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -50,6 +80,19 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 7,
+  },
+  customButton: {
+    backgroundColor: colors.orangeyellow,
+    width: 50,
+    height: 50,
+    borderRadius: 7,
+  },
+  smallImageStyles: {
+    width: 200,
+    height: 200,
+    margin: 10,
+    bordeWidth: 2,
+    borderColor: 'black',
   }
 
 
--- lesson3examples/app/config/colors.js
+++ lesson3examples/app/config/colors.js
@@ -1,3 +1,5 @@
 export default{
+    grassgreen:'#32a852',
+    orangeyellow:'#fcba03',
     
 }
